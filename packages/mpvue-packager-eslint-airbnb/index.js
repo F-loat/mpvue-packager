4d6edb867e9675464314eac649db5f57fa0ed985
F-loat <chaimaoyuan@foxmail.com>
2018-12-20 14:23:34 +0800

    feat: 添加 lint 配置示例

---
+++ packages/mpvue-packager-eslint-airbnb/index.js
@@ -0,0 +1,45 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: false,
+    node: true,
+    es6: true
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': require.resolve(`mpvue-packager-${process.env.MODE}-plugin/webpack.base.conf.js`)
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  },
+  globals: {
+    App: true,
+    Page: true,
+    wx: true,
+    getApp: true,
+    getPage: true,
+    requirePlugin: true
+  }
+}
